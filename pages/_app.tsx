807a349c9a81b065dc4d6bcea29e7ae3927eff45
blady <belazdzislaw@gmail.com>
2021-06-14 19:49:46 +0200

    Add Context and Wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import type { AppProps } from "next/app";
 import React from "react";
 
+import { AppWrapper } from "../context";
+
 import { Layout } from "../components/Layout/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppWrapper>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppWrapper>
   );
 }
 export default MyApp;
