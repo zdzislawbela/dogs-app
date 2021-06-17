44402c80b94414c3f299d53d26f4462febdd5499
blady <belazdzislaw@gmail.com>
2021-06-17 17:24:13 +0200

    Amend Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>🐕 Home</title>
+        <title>🐕</title>
         <meta
           name='description'
           content='Collecting open source dog pictures from dog.ceo'
