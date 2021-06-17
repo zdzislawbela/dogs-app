f463dca0d9322eedcdead65a7f713526e3943846
blady <belazdzislaw@gmail.com>
2021-06-17 14:54:07 +0200

    Amend Header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+
+import { KebabMenu } from "../KebabMenu/KebabMenu";
+import style from "./Header.module.css";
+
+export const Header = () => {
+  return (
+    <div className={style.header}>
+      <Link href='/'>
+        <div className={style.link}>
+          <a> Home 🏠</a>
+        </div>
+      </Link>
+
+      <Link href='/fetch'>
+        <div className={style.link}>
+          <a> Fetch 🐕</a>
+        </div>
+      </Link>
+
+      <Link href='/liked'>
+        <div className={style.link}>
+          <a> Liked ❤️</a>
+        </div>
+      </Link>
+
+      <KebabMenu />
+    </div>
+  );
+};
