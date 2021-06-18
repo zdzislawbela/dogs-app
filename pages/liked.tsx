45c873013c64a67a22b987d81bb4a9f32f5bcbfa
blady <belazdzislaw@gmail.com>
2021-06-18 12:05:26 +0200

    Amend liked page

--- pages/liked.tsx
+++ pages/liked.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import React from "react";
 import { LikedDogs } from "../components/LikedDogs/LikedDogs";
+import { ParticleBackground } from "../components/ParticleBackground/ParticleBackground";
+
 import Image from "next/image";
 import { useAppContext } from "../context";
 
@@ -10,7 +12,7 @@ export default function Liked() {
   const { likedDogs } = useAppContext();
 
   return (
-    <div>
+    <>
       <Head>
         <title>🐕 Liked!</title>
         <meta
@@ -18,20 +20,23 @@ export default function Liked() {
           content='Collecting open source dog pictures from dog.ceo'
         />
         <link rel='icon' href='/favicon.png' />
-      </Head>
+      </Head>{" "}
+      <ParticleBackground />
       {likedDogs.length !== 0 ? (
         <LikedDogs />
       ) : (
-        <div className={styles.container}>
-          Go to Fetch Tab and click:
-          <Image
-            src='/heart.png'
-            alt='Picture of heart'
-            width={24}
-            height={24}
-          />
-        </div>
+        <>
+          <div className={styles.container}>
+            Go to Fetch Tab and click:
+            <Image
+              src='/heart.png'
+              alt='Picture of heart'
+              width={24}
+              height={24}
+            />
+          </div>
+        </>
       )}
-    </div>
+    </>
   );
 }
