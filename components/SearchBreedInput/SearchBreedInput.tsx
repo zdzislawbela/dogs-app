faaa45c9b401d97721aeb68c372228e108125935
blady <belazdzislaw@gmail.com>
2021-06-20 20:35:32 +0200

    Add SearchBreedInput Component

--- components/SearchBreedInput/SearchBreedInput.tsx
+++ components/SearchBreedInput/SearchBreedInput.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./SearchBreedInput.module.css";
 
 export const SearchBreedInput = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <div className={style.inputContainer}>
       <input
         className={style.input}
         placeholder='Search'
         type='text'
-        value=''
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <div className={style.loupeContainer}>
         <img className={style.loupe} src='/loupe.png' alt='Loupe' />
