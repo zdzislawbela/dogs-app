40895d2a8bd35a0a69a8bc1ee30d63229d253bae
blady <belazdzislaw@gmail.com>
2021-06-23 00:50:15 +0200

    Refactor FetchedDogs component

--- components/FetchedDogs/FetchedDogs.tsx
+++ components/FetchedDogs/FetchedDogs.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 
-import { useAppContext } from "../../context";
+import { DogDetails, DogsDetails, useAppContext } from "../../context";
+
 import { HeartButton } from "../Buttons/HeartButton/HeartButton";
 import { LoadingSpinner } from "../LoadingSpinner/LoadingSpinner";
 
@@ -11,58 +12,38 @@ export const FetchedDogs = () => {
   const {
     loading,
     setLoading,
-    dogs,
-    setDogs,
-    breed,
-    setBreed,
-    error,
-    setError,
+    fetchedDogs,
+    setFetchedDogs,
+    setIsError,
+    apiCallCounter,
     setApiCallCounter,
-    dogBreedsContainer,
-    breedsChanged,
-    setBreedsChanged,
-    dogsAPI,
   } = useAppContext();
 
+  const [breed, setBreed] = useState("");
   const [random, setRandom] = useState(0);
+  const [storagedBreeds] = useLocalStorage("selectedBreeds", "");
 
-  const [breedsToStorage] = useLocalStorage("selectedBreeds", "");
-
-  const observer = useRef();
-
-  let userSelectedBreeds = dogBreedsContainer
-    .map((breed) => {
-      const storageIsNoSelection = breedsToStorage === "no-selection";
-      const storageIsEmptyString = breedsToStorage == "";
-      const localStorageIncludes = breedsToStorage.includes(breed.name);
-      if (storageIsEmptyString) {
-        return breed.name;
-      }
-      if (localStorageIncludes) {
-        return breed.name;
-      }
-    })
-    .filter((name) => {
-      return name != undefined;
-    });
+  const observer = useRef<null | IntersectionObserver>(null);
 
   const getRandomBreed = () => {
-    setRandom(Math.floor(Math.random() * userSelectedBreeds.length));
-    if ((random === 0) | (random === 1)) {
+    setRandom(Math.floor(Math.random() * storagedBreeds.length));
+
+    if (random === 0 || random === 1) {
       setRandom(-1);
     }
-    return setBreed(
-      userSelectedBreeds[random] !== undefined ? userSelectedBreeds[random] : ""
-    );
+
+    setBreed(storagedBreeds[random]);
+    return storagedBreeds[random];
   };
 
-  const lastBookElementRef = useCallback(
+  const useIntersectionObserver = useCallback(
     (node) => {
       if (loading) return;
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting) {
-          getRandomBreed();
+          const randomBreedFromSelected = getRandomBreed();
+          setBreed(randomBreedFromSelected);
         }
       });
       if (node) observer.current.observe(node);
@@ -70,59 +51,65 @@ export const FetchedDogs = () => {
     [loading]
   );
 
-  const getPosition = (string, subString, index) => {
+  const getPosition = (string: string, subString: string, index: number) => {
     return string.split(subString, index).join(subString).length;
   };
-
   useEffect(() => {
     getRandomBreed();
   }, []);
 
   useEffect(() => {
     setLoading(true);
-    setError(false);
+    setIsError(false);
+
     const fetchDog = async () => {
-      const response = await fetch(dogsAPI);
+      if (breed !== undefined) {
+        const response = await fetch(
+          `https://dog.ceo/api/breed/${breed}/images/random`
+        );
 
-      if (response.status !== 200) {
-        return setError("Something goes wrong. Please try again.");
+        if (response.status !== 200) {
+          return setIsError("Something goes wrong. Please try again.");
+        }
+        const newDog: DogDetails = await response.json();
+        const addNewDog: DogsDetails = [...fetchedDogs, newDog];
+        setFetchedDogs(addNewDog);
+
+        const addAPICall: number = apiCallCounter + 1;
+        setApiCallCounter(addAPICall);
       }
-
-      const newDog = await response.json();
-      setDogs((dogs) => [...dogs, newDog]);
-      setApiCallCounter((apiCallCounter) => apiCallCounter + 1);
     };
 
     const timeoutID = window.setTimeout(() => {
       fetchDog();
       setLoading(false);
-      setError(false);
+      setIsError(false);
     }, 100);
     observer.current;
 
     return () => window.clearTimeout(timeoutID);
-  }, [dogsAPI]);
+  }, [breed]);
 
   return (
     <div className={styles.dogsContainer}>
       <div className={styles.container}>
-        {dogs &&
-          dogs.map((dog, index) => {
+        {fetchedDogs &&
+          fetchedDogs.map((dog, index) => {
             const breedTitle = dog.message.slice(
               30,
               getPosition(dog.message, "/", 5)
             );
-
+            const isLast = fetchedDogs.length === index + 1;
             return (
               <div
                 key={dog.message}
                 className={styles.dog}
-                ref={dogs.length === index + 1 ? lastBookElementRef : null}
+                ref={useIntersectionObserver}
               >
                 <img className={styles.dogImg} src={dog.message} alt='dog' />
 
                 <div className={styles.titleContainer}>
-                  <HeartButton image={dog.message} breed={breedTitle} />
+                  <HeartButton message={dog.message} breed={breedTitle} />
 
                   <p className={styles.title}>{breedTitle}</p>
                 </div>
@@ -130,7 +117,7 @@ export const FetchedDogs = () => {
             );
           })}
 
-        <div className={styles.dog}>
+        <div className={styles.dog} ref={useIntersectionObserver}>
           <div className={styles.dogImg}>
             <img
               className={styles.dogImg}
