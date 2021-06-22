20d4c6a3bcd5d78b9038ef023bbaecb005ad5abb
blady <belazdzislaw@gmail.com>
2021-06-22 23:30:51 +0200

    Add TypeScript to LikedDogs

--- components/LikedDogs/LikedDogs.tsx
+++ components/LikedDogs/LikedDogs.tsx
@@ -6,23 +6,23 @@ import { RemoveFromLikeListButton } from "../Buttons/RemoveFromLikeListButton";
 import styles from "./LikedDogs.module.css";
 
 export const LikedDogs = () => {
-  const { likedDogs, setLikedDogs } = useAppContext();
+  const { likedDogs } = useAppContext();
 
   return (
     <div className={styles.container}>
       {likedDogs &&
         likedDogs.map((dog, index) => {
           return (
-            <div key={`${dog.image}${index}`} className={styles.dog}>
+            <div key={`${dog.message}${index}`} className={styles.dog}>
               <div className={styles.imgContainer}>
-                <img className={styles.dogImg} src={dog.image} alt='dog' />
+                <img className={styles.dogImg} src={dog.message} alt='dog' />
               </div>
 
               <div className={styles.dogImgTitle}>
                 <div className={styles.title}>
                   <p>{dog.breed}</p>
                 </div>
-                <RemoveFromLikeListButton image={dog.image} />
+                <RemoveFromLikeListButton image={dog.message} />
               </div>
             </div>
           );
