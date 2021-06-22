f7566b67abac01b20eab3713b3cc76636d98b6cf
blady <belazdzislaw@gmail.com>
2021-06-22 14:45:40 +0200

    Amend Heart component

--- components/Buttons/HeartButton/HeartButton.jsx
+++ components/Buttons/HeartButton/HeartButton.jsx
@@ -5,30 +5,21 @@ import style from "./HeartButton.module.css";
 
 export const HeartButton = ({ image, breed }) => {
   const { likedDogs, setLikedDogs } = useAppContext();
-  const [isLiked, setIsLiked] = useState(false);
-  const [isLikedStyle, setIsLikedStyle] = useState(false);
+
+  const images = likedDogs.map((dog) => dog.image);
+  const isLiked = images.includes(image);
 
   const handleLike = () => {
-    const images = likedDogs.map((dog) => dog.image);
-    const checkIsLiked = images.includes(image);
-
-    if (checkIsLiked) {
+    if (isLiked) {
       setLikedDogs(likedDogs.filter((dog) => dog.image !== image));
-      setIsLikedStyle(false);
-      setIsLiked(false);
-      return "";
+      return;
     }
-
     setLikedDogs((likedDogs) => [...likedDogs, { image, breed }]);
-    setIsLikedStyle(true);
-    setTimeout(() => {
-      setIsLiked(true);
-    }, 900);
   };
 
   return (
     <button onClick={handleLike} className={style.heartButton}>
-      {isLikedStyle && <div className={style.clickedHeartButton}></div>}
+      {isLiked && <div className={style.clickedHeartButton}></div>}
 
       <img
         src={isLiked ? "/likedHeart.png" : "/heart.png"}
